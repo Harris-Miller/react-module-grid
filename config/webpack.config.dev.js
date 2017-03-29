46f99d4192412c09bbcb6507fa4d45c6489d80a8
harris-miller <harrismillerconsulting@gmail.com>
2017-03-29 10:15:15 -0600

    beginnings

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const cssnext = require('postcss-cssnext');
+const cssImport require('postcss-import');
+const webpack = require('webpack');
+const getClientEnvironment = require('./env');
+const paths = require('./paths');
+
+module.exports = {
+
+};
